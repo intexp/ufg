a0398d2476db7567fd637d6f8de00fcae834c7d0
Giorgi Bichinashvili <giorgi.bichinashvili@gmail.com>
2015-08-11 12:40:43 +0400

    "Some commit"

---
+++ src/AppBundle/Menu/Builder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class Builder extends ContainerAware
+{
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $locale = $this->container->get('request_stack')->getCurrentRequest()->getLocale();
+
+        dump($locale);
+
+        $menu = $factory->createItem('root');
+
+        $menu->setChildrenAttributes(array('class' => 'nav navbar-nav'));
+
+        $menu->addChild('Home', array('route' => 'home'));
+
+        $menu->addChild('About us', array(
+            'route' => 'page',
+            'routeParameters' => array('slug' => 'about')
+        ));
+
+        $menu->addChild('Business', array('route' => 'company_index'));
+
+        $menu->addChild('Contact', array('route' => 'contact'));
+
+        return $menu;
+    }
+}
