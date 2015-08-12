eb472af7c32688ddf2635a4a121abbf728f31af8
Giorgi Bichinashvili <giorgi.bichinashvili@gmail.com>
2015-08-12 14:38:53 +0400

    Add dashboard blocks for slides, pages, companies and logged in user's stuff

--- src/AdminBundle/Controller/DashboardController.php
+++ src/AdminBundle/Controller/DashboardController.php
@@ -13,7 +13,38 @@ class DashboardController extends Controller
      */
     public function dashboardAction()
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $repository = $em->getRepository('AppBundle:Slide');
+        $slides = $repository->findBy(array(), array(), 3, 0);
+        $slidesCount = $repository->createQueryBuilder('x')
+            ->select('count(x.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+
+
+        $repository = $em->getRepository('AppBundle:Page');
+        $pages = $repository->findBy(array(), array(), 3, 0);
+        $pagesCount = $repository->createQueryBuilder('x')
+            ->select('count(x.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+
+
+        $repository = $em->getRepository('AppBundle:Company');
+        $companies = $repository->findBy(array(), array(), 3, 0);
+        $companiesCount = $repository->createQueryBuilder('x')
+            ->select('count(x.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+
         return $this->render('admin/dashboard.html.twig', array(
+            'slides' => $slides,
+            'slidesCount' => $slidesCount,
+            'pages' => $pages,
+            'pagesCount' => $pagesCount,
+            'companies' => $companies,
+            'companiesCount' => $companiesCount,
         ));
     }
 }
